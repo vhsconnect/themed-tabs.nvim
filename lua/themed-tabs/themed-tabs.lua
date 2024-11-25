e2e108d8715cc7e72ac916b82fe4d656c7d14546
vhsconnect <90valentin@gmail.com>
2024-11-25 23:31:19 +0100

    remove closed tabs from state

--- lua/themed-tabs/themed-tabs.lua
+++ lua/themed-tabs/themed-tabs.lua
@@ -7,7 +7,7 @@ function Plugin()
 		return vim.api.nvim_get_current_tabpage()
 	end
 
-	local function get_first_unused_scheme()
+	local function next_scheme()
 		local colors = config.config.colorschemes
 		for _, color in ipairs(colors) do
 			local matched = false
@@ -26,7 +26,6 @@ function Plugin()
 
 	local function on_enter()
 		local tab_id = id()
-
 		if next(State) == nil then
 			State[tab_id] = { colorscheme = vim.g.colors_name }
 			return
@@ -37,7 +36,7 @@ function Plugin()
 			return
 		end
 
-		local new_colorscheme = get_first_unused_scheme()
+		local new_colorscheme = next_scheme()
 		vim.cmd("colorscheme " .. new_colorscheme)
 		State[tab_id] = { colorscheme = new_colorscheme }
 	end
@@ -47,12 +46,32 @@ function Plugin()
 		State[tab_id] = { colorscheme = vim.g.colors_name }
 	end
 
-	return { on_enter = on_enter, on_leave = on_leave }
+	local function on_tab_closed()
+		local current_tabs = vim.api.nvim_list_tabpages()
+
+		local live_tabs = {}
+		for _, tab in ipairs(current_tabs) do
+			live_tabs[tab] = true
+		end
+
+		for tab_id, _ in pairs(State) do
+			if not live_tabs[tab_id] then
+				State[tab_id] = nil
+			end
+		end
+	end
+
+	return {
+		on_enter = on_enter,
+		on_leave = on_leave,
+		on_tab_closed = on_tab_closed,
+	}
 end
 
 local plugin = Plugin()
 vim.api.nvim_create_autocmd("TabEnter", { callback = plugin.on_enter })
 vim.api.nvim_create_autocmd("TabLeave", { callback = plugin.on_leave })
+vim.api.nvim_create_autocmd("TabClosed", { callback = plugin.on_tab_closed })
 
 local M = {}
 M.setup = config.setup
